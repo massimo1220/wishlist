d2c7fff78d51ae223ec9cb29b203f663f2f7788f
massimo1220 <dreamskydev@gmail.com>
2023-12-11 00:39:49 +0100

    folder strucutre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishListComponent } from './add-wish-list/add-wish-list.component';
-import { WishFilterComponent } from './wish-filter/wish-filter.component';
-import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
+import { WishModule } from './wish/wish.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WishListComponent,
-    AddWishListComponent,
-    WishFilterComponent,
-    WishListItemComponent,
-  ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WishModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/wish/wish.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { WishListComponent } from './wish-list/wish-list.component';
+import { AddWishListComponent } from './add-wish-list/add-wish-list.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
+import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
+
+@NgModule({
+  declarations: [
+    WishListComponent,
+    AddWishListComponent,
+    WishFilterComponent,
+    WishListItemComponent,
+  ],
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  exports: [
+    WishListComponent,
+    AddWishListComponent,
+    WishFilterComponent,
+    WishListItemComponent,
+  ]
+})
+export class WishModule {}
