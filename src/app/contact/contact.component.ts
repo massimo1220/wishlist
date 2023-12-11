91c81cfca9d0fbcfbec179c45229dbab41e6ac72
massimo1220 <dreamskydev@gmail.com>
2023-12-11 13:10:33 +0100

    Final

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator([
+  'gmail.com',
+  'yahoo.com',
+  'hotmail.com'
+]);
 
 @Component({
   selector: 'app-contact',
@@ -9,7 +16,11 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 export class ContactComponent {
   contactForm = new FormGroup({
     senderName: new FormControl('', Validators.required),
-    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      invalidEmailDomain,
+    ]),
     senderMessage: new FormControl('', [
       Validators.required,
       Validators.minLength(10),
