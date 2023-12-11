50d3ef9b1bfecb850b31f81358776e136820655c
massimo1220 <dreamskydev@gmail.com>
2023-12-11 10:32:15 +0100

    form control

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
 
+  submitForm() {
+    console.log(this.contactForm.valid);
+
+    // if (this.senderEmailControl.dirty) {
+    //   alert('you changed the name fiedl');
+    // }
+  }
 }
